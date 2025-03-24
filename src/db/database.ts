0ce13ddabf5efad0a0d0217c98852595fab7cc4a
damienn7 <damien.nicolleau@epitech.eu>
2025-03-24 07:50:13 +0100

    ref #1 - Added environment variables and database configuration

---
+++ src/db/database.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: process.env.DB_TYPE as "postgres" | "mysql" | "mariadb",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    logging: true,
+    synchronize: true,
+    entities: ["src/db/models/*.{js,ts}"],
+    // subscribers: [],
+    // migrations: [],
+});
